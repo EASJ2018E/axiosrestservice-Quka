1edb25a392581ac4e760a054ccc049ecada12f12
Arlind Ukshini <quka93@gmail.com>
2018-10-29 10:03:48 +0100

    post customer

--- src/js/index.ts
+++ src/js/index.ts
@@ -9,7 +9,11 @@ interface ICustomer {
 
 let uri: string = 'https://restcustomerservice20181011112426.azurewebsites.net/api/Customer';
 let customers: ICustomer[] = [];
-let HTMLContent = document.getElementById("content");
+let HTMLContent = document.getElementById("output");
+
+function updateHtml(customers: ICustomer[]): void {
+    HTMLContent.innerHTML = JSON.stringify(customers);
+}
 
 /**
  * Axios get request
@@ -22,12 +26,45 @@ function getHttp(): void {
         customers = response.data;
         
         console.log(customers);
-        HTMLContent.innerHTML = JSON.stringify(customers);
-
+        updateHtml(customers);
     })
     .catch(function (error) {
         console.log(error);
     });
 }
 
+
+/**
+ * Axios post request
+ */
+function postHttp(): void {
+    //let idVal: number = Number( (<HTMLInputElement> document.getElementById("id")).value );
+    let firstNameVal: string = (<HTMLInputElement> document.getElementById("firstName")).value;
+    let lastNameVal: string = (<HTMLInputElement> document.getElementById("lastName")).value;
+    let yearVal: number = Number((<HTMLInputElement> document.getElementById("year")).value);
+
+    let newCustomer: ICustomer = <ICustomer> {
+        firstName: firstNameVal,
+        lastName: lastNameVal,
+        year: yearVal
+    }
+
+    axios.post<ICustomer>(uri, newCustomer)
+    .then(function(response: AxiosResponse<ICustomer>): void {
+        if(response.status == 200) {
+            console.log(response.data);
+            let customerResult: ICustomer = response.data;
+            
+            customers.push(customerResult);
+
+            updateHtml(customers);
+        }
+    })
+    .catch(function(error){
+        console.log(error);
+    });
+}
+
 getHttp();
+
+document.getElementById("postHttpBtn").addEventListener("click", postHttp);
